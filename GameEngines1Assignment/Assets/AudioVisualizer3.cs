d565eeee88d7f6ec1643d6b7392f0d0e57faab84
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2020-12-20 21:20:50 +0000

    Audio Reactive buildings added

---
+++ GameEngines1Assignment/Assets/AudioVisualizer3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizer3 : MonoBehaviour
+{
+    public int band = 1;
+    public float scale = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update () {
+       
+        Vector3 ls = transform.localScale;
+        ls.y = Mathf.Lerp(ls.y, 1 + (AudioAnalyzer.bands[band] * scale), Time.deltaTime * 3.0f);
+        transform.localScale = ls;
+
+	}
+}
