f962ecf92f2c1b046af6757a0db31c08405bc92a
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2020-12-22 23:28:40 +0000

    Added suspension to car

---
+++ GameEngines1Assignment/Assets/Suspension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspension : MonoBehaviour
+{
+
+    public float speed = 1f;
+    //adjust this to change how high it goes   
+    public float height = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+            Vector3 pos = transform.position;
+            //calculate what the new Y position will be
+            float newY = Mathf.Sin(Time.time * speed);
+            //set the object's Y to the new calculated Y
+            transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
+}
