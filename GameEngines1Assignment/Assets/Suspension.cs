432a47f5167ed01f48a42f8f4e39a1314cf4b1d2
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2020-12-22 23:47:06 +0000

    Adjusted Suspension

--- GameEngines1Assignment/Assets/Suspension.cs
+++ GameEngines1Assignment/Assets/Suspension.cs
@@ -11,15 +11,17 @@ public class Suspension : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
-            Vector3 pos = transform.position;
+            Vector3 pos = this.transform.position;
             //calculate what the new Y position will be
             float newY = Mathf.Sin(Time.time * speed);
             //set the object's Y to the new calculated Y
-            transform.position = new Vector3(pos.x, newY, pos.z) * height;
+            newY = newY + 4;
+            newY = (newY * height);
+            transform.position = new Vector3(pos.x, newY, pos.z);
     }
 }
