d6e7388f7c96b25f311827aa8d29d496cd2099e4
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2020-12-23 19:21:18 +0000

    Added Hue changing script

--- GameEngines1Assignment/Assets/HueScript.cs
+++ GameEngines1Assignment/Assets/HueScript.cs
@@ -1,30 +1,39 @@
-﻿/*
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
 
 public class HueScript : MonoBehaviour
 {
+    public float colorShift = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        var volume = this.GetComponent<Volume>();
-
-        if (volume.profile.TryGet<Bloom>(out var bloom))
-        {
-            bloom.intensity.overrideState = true;
-            bloom.intensity.value = 2f;
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        var volume = this.GetComponent<PostProcessVolume>();
+
+        if (volume.profile.TryGetSettings<ColorGrading>(out var ColorGrading))
+        {
+            if(ColorGrading.hueShift.value == 180)
+            {
+                ColorGrading.hueShift.value = 1;
+            }
+            else
+            {
+                ColorGrading.hueShift.value += (float)colorShift;
+            }
+
+        }
         
     }
+    
 }
-*/
+
  
