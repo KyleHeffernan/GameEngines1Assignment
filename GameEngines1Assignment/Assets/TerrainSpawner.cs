c01dc97bebccbfb7d4bedbbe21272d6f27e426b7
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2020-12-23 14:51:08 +0000

    Added varying prefabs

--- GameEngines1Assignment/Assets/TerrainSpawner.cs
+++ GameEngines1Assignment/Assets/TerrainSpawner.cs
@@ -6,11 +6,18 @@ public class TerrainSpawner : MonoBehaviour
 {
 
     public GameObject terrainPrefab;
+
+    public GameObject terrainPrefab2;
+
+    private GameObject terrainPrefabCopy;
+
     public float speed = 1;
     private float audioSpeed;
     public GameObject player;
     public float moveDistance = 10;
     public float spawnDistance = 100;
+
+    private int counter;
     
 
     // Start is called before the first frame update
@@ -34,8 +41,17 @@ public class TerrainSpawner : MonoBehaviour
 
     void Spawn()
     {
-        
-        GameObject terrain = GameObject.Instantiate<GameObject>(terrainPrefab, transform.position, transform.rotation);
+        counter++;
+        //random = Random.Range(1,5);
+        if(counter % 5 == 0)
+        {
+            terrainPrefabCopy = terrainPrefab;
+        }
+        else
+        {
+            terrainPrefabCopy = terrainPrefab2;
+        }
+        GameObject terrain = GameObject.Instantiate<GameObject>(terrainPrefabCopy, transform.position, transform.rotation);
         MoveTerrain moveScript = terrain.AddComponent<MoveTerrain>();
         moveScript.speed = speed;
         transform.position = transform.position + (-transform.forward * (moveDistance * terrain.transform.localScale.x));
